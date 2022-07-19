4e82c870689ec0ee6f9709c63487b0670ae40d73
Tanya Buechel <tanyabuechel@gmail.com>
2022-07-19 11:29:56 -0500

    Finished Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Awesome Pictures';
+  image1 = 'https://imageio.forbes.com/specials-images/imageserve/62d26b610a00ec80dc9139f3/The-Southern-Ring-Nebula--as-captured-by-the-James-Webb-Space-Telescope--JWST--/960x0.jpg?format=jpg&width=960';
+  image2 = 'https://i.guim.co.uk/img/media/60f8262ffcf0949c3c439cbc1ff26e30ac5cdc36/0_0_4537_4630/master/4537.jpg?width=1020&quality=85&fit=max&s=c8abb0b5a371966a683846afec8bb5d7';
+  image3 = 'http://www.nasa.gov/sites/default/files/thumbnails/image/web_first_images_release.png';
 
   constructor() { }
 
